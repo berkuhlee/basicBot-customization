ab33c185cf81dcb2b71798cc31cabc1b26c55594
berkuhlee <berkuhlee@gmail.com>
2014-12-14 01:04:33 -0800

    Update extension.js
    autodisable: false

--- extension.js
+++ extension.js
@@ -67,7 +67,7 @@
         maximumCycletime: 10,
         timeGuard: true,
         maximumSongLength: 7,
-        autodisable: true,
+        autodisable: false,
         commandCooldown: 30,
         usercommandsEnabled: true,
         lockskipPosition: 3,
